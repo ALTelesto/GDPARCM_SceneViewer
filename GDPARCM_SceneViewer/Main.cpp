cf901a9c0673bf2b9875a7eb51bade482f3ab885
ALTelesto <joachim.arguelles@gmail.com>
2025-03-31 22:38:10 +0800

    updated Model implementation

--- GDPARCM_SceneViewer/Main.cpp
+++ GDPARCM_SceneViewer/Main.cpp
@@ -31,7 +31,6 @@ using namespace std;
 // important in order for Model3D's draw function
 GLuint shaderProgram;
 std::vector<GLuint> mesh_indices;
-GLuint VAO, VBO, EBO;
 
 // camera position modifier
 glm::vec3 pos_mod = glm::vec3(0.0f, 0.0f, 0.0f);
@@ -58,34 +57,55 @@ double cooldownTime = 3.0;
 double lastSpawnTime = -3.0;
 double currentTime = 0.0;
 
-// Model3D class
-class Model3D {
+// Model class
+class Model {
 private:
     glm::vec3 position;
     glm::vec3 rotation;
     glm::vec3 scale;
+    GLuint VAO, VBO, EBO;
 
 public:
-    Model3D(glm::vec3 position, glm::vec3 rotation, glm::vec3 scale) {
+    Model(glm::vec3 position, glm::vec3 rotation, glm::vec3 scale, const std::vector<GLfloat>& vertices, const std::vector<GLuint>& indices) {
         this->position = position;
         this->rotation = rotation;
         this->scale = scale;
+
+        // Initialize VAO, VBO, and EBO
+        glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &VBO);
+        glGenBuffers(1, &EBO);
+
+        glBindVertexArray(VAO);
+
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(GLfloat), vertices.data(), GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)0);
+        glEnableVertexAttribArray(0);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLuint), indices.data(), GL_STATIC_DRAW);
+
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindVertexArray(0);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
     }
 
-    // code was ripped from the while() loop in main() and placed into draw() function
-    void draw() {
+    ~Model() {
+        glDeleteVertexArrays(1, &VAO);
+        glDeleteBuffers(1, &VBO);
+        glDeleteBuffers(1, &EBO);
+    }
 
+    void draw() {
         // axis
         float axis_x = 0.0;
         float axis_y = 1.0;
         float axis_z = 0.0;
 
         // camera stuff
-        glm::mat4 cameraPosMatrix = glm::translate(
-            glm::mat4(1.0f),
-            pos_mod * -1.0f
-        );
-
+        glm::mat4 cameraPosMatrix = glm::translate(glm::mat4(1.0f), pos_mod * -1.0f);
         cameraPosMatrix = glm::rotate(cameraPosMatrix, glm::radians(rx_mod), glm::normalize(glm::vec3(axis_x, axis_y, axis_z)));
         cameraPosMatrix = glm::rotate(cameraPosMatrix, glm::radians(ry_mod), glm::normalize(glm::vec3(axis_y, axis_x, axis_z)));
 
@@ -99,17 +119,12 @@ public:
 
         glm::mat4 viewMatrix = glm::lookAt(pos_mod, cameraCenter, worldUp);
         glm::mat4 projection = glm::perspective(glm::radians(fov_mod), 600.0f / 600.0f, 0.1f, 100.0f);
-        // end of camera stuff
 
         // applying of model transformation
-        // translate -> rotate -> scale
         glm::mat4 transformation_matrix = glm::translate(glm::mat4(1.0f), this->position);
-
         transformation_matrix = glm::rotate(transformation_matrix, glm::radians(this->rotation.x), glm::normalize(glm::vec3(axis_x, axis_y, axis_z)));
         transformation_matrix = glm::rotate(transformation_matrix, glm::radians(this->rotation.y), glm::normalize(glm::vec3(axis_y, axis_x, axis_z)));
-
         transformation_matrix = glm::scale(transformation_matrix, this->scale);
-        // end of applying of model transformation 
 
         // sending transformation matrix to shader program
         unsigned int viewLoc = glGetUniformLocation(shaderProgram, "view");
@@ -121,22 +136,15 @@ public:
         unsigned int projLoc = glGetUniformLocation(shaderProgram, "projection");
         glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
-        //set color
+        // set color
         glm::vec4 FragColorIn = glm::vec4(0.0, 1.0, 0.0, 1.0);
         unsigned int colorLoc = glGetUniformLocation(shaderProgram, "FragColorIn");
         glUniform4fv(colorLoc, 1, glm::value_ptr(FragColorIn));
-        // end of sending transformation matrix to shader program
 
         // rendering
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawElements(
-            GL_TRIANGLES,
-            mesh_indices.size(),
-            GL_UNSIGNED_INT,
-            0
-        );
-        // end
+        glDrawElements(GL_TRIANGLES, mesh_indices.size(), GL_UNSIGNED_INT, 0);
     }
 
     void setPosition(glm::vec3 position) {
@@ -150,17 +158,10 @@ public:
     }
 };
 
-// we store the instances of Model3D here
-vector<Model3D*> vecModels;
+// we store the instances of Model here
+vector<Model*> vecModels;
 
-void Key_Callback(
-    GLFWwindow* window,
-    int key,
-    int scancode,
-    int action,
-    int mod
-) {
-
+void Key_Callback(GLFWwindow* window, int key, int scancode, int action, int mod) {
     // get cameraCenter aka where the camera is currently pointed at
     glm::vec3 cameraCenter;
     cameraCenter.x = cos(glm::radians(rx_mod)) * cos(glm::radians(ry_mod));
@@ -190,16 +191,30 @@ void Key_Callback(
     if (key == GLFW_KEY_SPACE && action == GLFW_PRESS && (currentTime - lastSpawnTime) > cooldownTime) {
         lastSpawnTime = glfwGetTime();
         glm::vec3 ObjectPos = (spawnMod * cameraCenter) + pos_mod;
-        Model3D* newModel = new Model3D(ObjectPos, glm::vec3(0.f, 0.f, 0.f), glm::vec3(scale_mod, scale_mod, scale_mod));
-        vecModels.push_back(newModel);
+
+        //loading of mesh
+        std::string path = "3D/kacsrr3.obj";
+        std::vector<tinyobj::shape_t> shape;
+        std::vector<tinyobj::material_t> material;
+        std::string warning, error;
+
+        tinyobj::attrib_t attributes;
+
+        bool success = tinyobj::LoadObj(&attributes, &shape, &material, &warning, &error, path.c_str());
+
+        for (int i = 0; i < shape[0].mesh.indices.size(); i++) {
+            mesh_indices.push_back(shape[0].mesh.indices[i].vertex_index);
+        }
+
+        //creation of model
+        Model* newModel = new Model(ObjectPos, glm::vec3(0.f, 0.f, 0.f), glm::vec3(scale_mod, scale_mod, scale_mod), attributes.vertices, mesh_indices);
+
+
+    	vecModels.push_back(newModel);
     }
 }
 
-void Mouse_Callback(
-    GLFWwindow* window,
-    double mouseXNew,
-    double mouseYNew
-) {
+void Mouse_Callback(GLFWwindow* window, double mouseXNew, double mouseYNew) {
     // initialize mouse position
     if (start) {
         mouseX = mouseXNew;
@@ -226,8 +241,7 @@ void Mouse_Callback(
     }
 }
 
-int main(void)
-{
+int main(void) {
     std::cout << "Dear ImGui version: " << IMGUI_VERSION << std::endl;
 
     // from this point on, nothing differs from the code from previous lessons
@@ -269,8 +283,7 @@ int main(void)
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(800, 600, "Hello World", NULL, NULL);
-    if (!window)
-    {
+    if (!window) {
         glfwTerminate();
         return -1;
     }
@@ -302,9 +315,7 @@ int main(void)
     bool show_another_window = false;
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
-
-    //shaders
-
+    // shaders
     std::fstream vertSrc("Shaders/sample.vert");
     std::stringstream vertBuff;
     vertBuff << vertSrc.rdbuf();
@@ -331,71 +342,10 @@ int main(void)
 
     glLinkProgram(shaderProgram);
 
-    std::string path = "3D/kacsrr3.obj";
-    std::vector<tinyobj::shape_t> shape;
-    std::vector<tinyobj::material_t> material;
-    std::string warning, error;
-
-    tinyobj::attrib_t attributes;
-
-    bool success = tinyobj::LoadObj(
-        &attributes,
-        &shape,
-        &material,
-        &warning,
-        &error,
-        path.c_str()
-    );
-
-    for (int i = 0; i < shape[0].mesh.indices.size(); i++) {
-        mesh_indices.push_back(
-            shape[0].mesh.indices[i].vertex_index
-        );
-    }
-
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
-
-    //text
-    glBindVertexArray(VAO);
-    //text
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(
-        GL_ARRAY_BUFFER,
-        sizeof(GLfloat) * attributes.vertices.size(),
-        &attributes.vertices[0],
-        GL_STATIC_DRAW
-    );
-
-    glVertexAttribPointer(
-        0,
-        3, //xyz
-        GL_FLOAT,
-        GL_FALSE,
-        3 * sizeof(GL_FLOAT),
-        (void*)0
-    );
-    glEnableVertexAttribArray(0);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(
-        GL_ELEMENT_ARRAY_BUFFER,
-        sizeof(GLuint) * mesh_indices.size(),
-        mesh_indices.data(),
-        GL_STATIC_DRAW
-    );
-
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
     /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
+    while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
-        if (glfwGetWindowAttrib(window, GLFW_ICONIFIED) != 0)
-        {
+        if (glfwGetWindowAttrib(window, GLFW_ICONIFIED) != 0) {
             ImGui_ImplGlfw_Sleep(10);
             continue;
         }
@@ -411,11 +361,11 @@ int main(void)
         ImGui::Render();
         glClear(GL_COLOR_BUFFER_BIT);
 
-        //get current timestamp
+        // get current timestamp
         currentTime = glfwGetTime();
 
-        //loop through vector and call draw function from each Model3D instances
-        for (Model3D* Model : vecModels) {
+        // loop through vector and call draw function from each Model instances
+        for (Model* Model : vecModels) {
             Model->draw();
         }
 
@@ -427,10 +377,6 @@ int main(void)
         /* Poll for and process events */
         glfwPollEvents();
     }
-
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
-    glDeleteBuffers(1, &EBO);
 
     glfwTerminate();
     return 0;
